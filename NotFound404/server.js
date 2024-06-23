c6e9fd34508e068f07994dbb04aeb45a0c66e35a
Lucia Viazzo <viazzo94@gmail.com>
2024-06-23 19:04:21 -0300

    Aplico modificaciones sugeridas

--- NotFound404/server.js
+++ NotFound404/server.js
@@ -5,22 +5,23 @@ const app = express();
 const port = 3001;
 const httpServer = http.createServer(app);
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+//No es necesario
+//app.use(express.urlencoded({extended: true}));
+//app.use(express.json());
 
 //Configuracion ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-app.use('7static', express.static('views'));
+app.use(express.static('views'));
 
 //Ruta correcta 
 app.get('/', (_req, res) => {
-  res.render('index');
+  return res.render('index');
 });
 
 //Rutas inexistentes
 app.use((_req, res) => {
-  res.status(404).render('404');
+  return res.status(404).render('404');
 });
 
 //Incio el servidor
