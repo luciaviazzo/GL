34879adcb55dfcb7e03ce0c73e8031bceeeafb01
Lucia Viazzo <viazzo94@gmail.com>
2024-07-17 14:13:55 -0300

    index.js

--- Login/scripts/index.js
+++ Login/scripts/index.js
@@ -1,17 +1,34 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //Captura los imputs
     const email = document.querySelector('#email')
     const password = document.querySelector('#password')
     const form = document.querySelector('form')
 
     form.addEventListener('submit', (event) => {
 
+        //Previene comportamiento por defecto
         event.preventDefault()
 
+        //Captura los valores de email y password
         const data = {
             email: email.value,
             password: password.value
         }
 
-        fetch('http://localhost:3000/api/paintings')
+        //Manda la peticion a la url
+        //Manda el header y el body con la data (email y password)
+        fetch('http://localhost:3000/api/paintings', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application.json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then((data) => {
+                window.localStorage.setItem('token', data.token)
+                window.location.href = '/pages/admin.html'
+            })
+            .catch(error => console.log(error))
     })
 })
