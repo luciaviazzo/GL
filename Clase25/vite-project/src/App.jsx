c2ff6eaf368d918f8c265a332461278c6b2250e1
Lucia Viazzo <viazzo94@gmail.com>
2024-07-18 23:09:09 -0300

    solved exercise

--- Clase25/vite-project/src/App.jsx
+++ Clase25/vite-project/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-          signal: abortController.signal //Controla cuando cancelar la transaccion
+          signal: abortControllerRef.current.signal //Controla cuando cancelar la transaccion
         })
 
         if (!response.ok) {
@@ -26,6 +26,7 @@ function App() {
           return
         }
         const data = await response.json()
+        setData(data)
 
         setData(response)
       } catch (error) {
@@ -35,32 +36,38 @@ function App() {
         setIsLoading(false)
       }
     }
-  },[])
+    //Cada vez que se rerenderiza el componente no se genera una nueva señal 
+    abortControllerRef.current = new AbortController()
+    fetchData()
+
+    //Se desmonta el componente
+    return () => (
+      abortControllerRef.current.abort()
+    )
+  }, [])
 
 
+  if (isLoading) {
+    return (
+      <main>Cargando datos...</main>
+    )
+  }
 
+  if (error !== '') {
+    return (
+      <main>{error}</main>
+    )
+  }
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Cargando datos...</p>
-      ) : (
-        <div>
-          {error ? (
-            <p>{error}</p>
-          ) : (
-            <div>
-              <h1>Publicaciones:</h1>
-              <ul>
-                {data.map(post => (
-                  <li key={post.id}>{post.title}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+    <main>
+      <h1>Publicaciones:</h1>
+      <ul>
+        {data.length > 0 ? map(post => (
+          <li key={post.id}>{post.title}</li>
+        )) : null}
+      </ul>
+    </main >
   )
 }
 
