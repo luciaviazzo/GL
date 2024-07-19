20e9e4e76a0dfa9c5b7bce915fa59c6e759b57cf
Lucia Viazzo <viazzo94@gmail.com>
2024-07-18 22:16:49 -0300

    first version

--- Clase25/vite-project/src/App.jsx
+++ Clase25/vite-project/src/App.jsx
@@ -1,34 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let abortController
+
+  fetchData()
+
+  const fetchData = async () => {
+    abortController = new AbortController()
+
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        signal: abortController.signal
+      })
+
+      if (!response.ok) {
+        console.log('Error al realizar la solicitud')
+        return
+      }
+
+      setData(response)
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error.message)
+      setError('Error al cargar los datos. Por favor, intenta nuevamente.')
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {isLoading ? (
+        <p>Cargando datos...</p>
+      ) : (
+        <div>
+          {error ? (
+            <p>{error}</p>
+          ) : (
+            <div>
+              <h1>Publicaciones:</h1>
+              <ul>
+                {data.map(post => (
+                  <li key={post.id}>{post.title}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   )
 }
 
